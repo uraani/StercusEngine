d2f093384330610cc07143bc1bee637a661a6e59
Sami Moisio <moisios@gmail.com>
2016-01-28 04:17:33 +0200

    fixing all projects equally

---
+++ zlibVC/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Uraani/Downloads/sandox/zlib-1.2.8
+# Build directory: C:/Users/Uraani/Downloads/sandox/zlibVC
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
