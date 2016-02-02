ead00de606d5b89afaf9ef395941eb55ccfc1b10
Sami Moisio <moisios@gmail.com>
2016-02-02 07:37:32 +0200

    for some reason i could finally compile from source so now i got rid of a bunch of static libs

--- SGLdemo/demo.cpp
+++ SGLdemo/demo.cpp
@@ -4,7 +4,7 @@ int main(int argc, char* argv[])
 {
 	SGL_Init();
 	SGL_Window* window = SGL_CreateWindow("SGLDemo", 3, 3, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE|SDL_WINDOW_OPENGL);
-	SGL_RunGLTest(window);
+	//SGL_RunGLTest(window);
 	SDL_Event e = SDL_Event();
 	for (; e.type != SDL_QUIT && e.type != SDL_KEYDOWN; SDL_PollEvent(&e));
 	SGL_Quit();
